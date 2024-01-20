5d313b541a3b6696d382f0384d0ef6dc410d2131
naitosama <naitosama@qq.com>
2024-01-20 11:57:39 +0800

    in development...

--- lib/boss.dart
+++ lib/boss.dart
@@ -11,27 +11,61 @@ class _bossPageState extends State<bossPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('主页面'),
-      ),
+      // appBar: AppBar(
+      //   title: Text('主页面'),
+      // ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           // 公告栏
-          Container(
-            color: Colors.blue,
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              '最新公告: Flutter 主页面示例',
-              style: TextStyle(color: Colors.white),
+          // Container(
+          //   color: Colors.blue,
+          //   padding: EdgeInsets.all(16.0),
+          //   child: Text(
+          //     '最新公告: Flutter 主页面示例',
+          //     style: TextStyle(color: Colors.white),
+          //   ),
+          // ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              primary: false,
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 8),
+                  child: Container(
+                    width: MediaQuery.sizeOf(context).width * 0.8,
+                    height: MediaQuery.sizeOf(context).height * 0.3,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 4,
+                          color: Color(0x520E151B),
+                          offset: Offset(0, 2),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: ListView(
+                      padding: EdgeInsets.zero,
+                      scrollDirection: Axis.vertical,
+                      children: [],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           // Boss 状态格子
-          BossStatusTile(bossName: 'Boss 1'),
-          BossStatusTile(bossName: 'Boss 2'),
-          BossStatusTile(bossName: 'Boss 3'),
-          BossStatusTile(bossName: 'Boss 4'),
-          BossStatusTile(bossName: 'Boss 5'),
+          bossCard(bossName: 'Boss 1',bossImg: 'images/1.webp',),
+          bossCard(bossName: 'Boss 2',bossImg: 'images/2.webp',),
+          bossCard(bossName: 'Boss 3',bossImg: 'images/3.webp',),
+          bossCard(bossName: 'Boss 4',bossImg: 'images/4.webp',),
+          bossCard(bossName: 'Boss 5',bossImg: 'images/5.webp',),
         ],
       ),
     );
@@ -62,3 +96,88 @@ class BossStatusTile extends StatelessWidget {
     );
   }
 }
+
+class bossCard extends StatefulWidget {
+  final String bossName;
+  final String bossImg;
+  const bossCard({super.key, required this.bossName, required this.bossImg});
+
+  @override
+  State<bossCard> createState() => _bossCardState();
+}
+
+class _bossCardState extends State<bossCard> {
+  late String bossName;
+  late String bossImg;
+  @override
+  void initState() {
+    super.initState();
+    bossName = widget.bossName;
+    bossImg = widget.bossImg;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return // Generated code for this Container Widget...
+      Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 8),
+        child: Container(
+          width: double.infinity,
+          height: 80,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 4,
+                color: Color(0x520E151B),
+                offset: Offset(0, 2),
+              )
+            ],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Image.asset(
+                    bossImg,
+                    width: 80,
+                    height: 80,
+                    // fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        LinearProgressIndicator(
+                          value: 0.5,
+                          backgroundColor: Colors.grey[200],
+                          valueColor: AlwaysStoppedAnimation(Colors.blue),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 0),
+                          child: Text(
+                            bossName,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  }
+}
+
